32030459a67dba3f97a1ca3bedf24c80133c1366
JiauZhang <1743960454@qq.com>
2025-04-13 12:06:23 +0800

    add qwen2 rotary position embedding

--- nnm/layers/positional.py
+++ nnm/layers/positional.py
@@ -8,14 +8,18 @@ class RoPE(nn.Module):
         self.max_seq_len = max_seq_len
         self.embed_dim = embed_dim
         self.base = base
-        theta = 1.0 / (base ** (torch.arange(0, embed_dim, 2, dtype=torch.float64) / embed_dim))
+        self.precompute()
+
+    @torch.no_grad()
+    def precompute(self):
+        theta = 1.0 / (self.base ** (torch.arange(0, self.embed_dim, 2, dtype=torch.float64) / self.embed_dim))
         theta = theta.reshape(-1, 1).repeat(1, 2).reshape(-1)
-        m = torch.arange(max_seq_len, device=theta.device, dtype=torch.float64)
+        m = torch.arange(self.max_seq_len, device=theta.device, dtype=torch.float64)
         m_theta = torch.outer(m, theta)
         self.cos = torch.cos(m_theta).to(dtype=torch.float32)
         # [-1, 1, -1, 1, ...]
         m_theta = (m_theta.reshape(-1, 2) * torch.tensor([-1, 1], dtype=torch.float64)).reshape(
-            max_seq_len, embed_dim
+            self.max_seq_len, self.embed_dim
         )
         self.sin = torch.sin(m_theta).to(dtype=torch.float32)
 
@@ -26,4 +30,26 @@ class RoPE(nn.Module):
         sin_pe = self.sin[:shape[-2]]
         cos_pe = self.cos[:shape[-2]]
         y = x * cos_pe + x.reshape(-1, 2).flip(dims=[-1]).reshape(shape) * sin_pe
+        return y
+
+class QwenRoPE(RoPE):
+    @torch.no_grad()
+    def precompute(self):
+        theta = 1.0 / (self.base ** (torch.arange(0, self.embed_dim, 2, dtype=torch.float64) / self.embed_dim))
+        m = torch.arange(self.max_seq_len, device=theta.device, dtype=torch.float64)
+        # [max_seq_len, embed_dim // 2]
+        m_theta = torch.outer(m, theta)
+        # [max_seq_len, embed_dim]
+        self.sin = torch.sin(m_theta).to(dtype=torch.float32).repeat(1, 2)
+        self.cos = torch.cos(m_theta).to(dtype=torch.float32).repeat(1, 2)
+
+    def forward(self, x):
+        shape = x.shape
+        half_embed_dim = shape[-1] // 2
+        x1 = x[..., :half_embed_dim]
+        x2 = x[..., half_embed_dim:]
+        y = torch.cat((-x2, x1), dim=-1)
+        sin_pe = self.sin[:shape[-2]]
+        cos_pe = self.cos[:shape[-2]]
+        y = (x * cos_pe) + (y * sin_pe)
         return y
--- tests/test_positional.py
+++ tests/test_positional.py
@@ -1,5 +1,6 @@
 import torch, pytest
-from nnm.layers.positional import RoPE
+from torch import nn
+from nnm.layers.positional import RoPE, QwenRoPE
 
 def llama_precompute_freqs_cis(dim, end, theta=10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
@@ -40,3 +41,58 @@ def test_rope(seq_len, embed_dim, max_seq_len, base):
 
     assert y.shape == llama_y.shape
     assert torch.abs(llama_y - y).mean() < 1e-5
+
+# https://github.com/huggingface/transformers/blob/953196a43dae6a3c474165fba7d215fcbc7b7730/src/transformers/models/qwen2/modeling_qwen2.py#L336
+class Qwen2RotaryEmbedding(nn.Module):
+    def __init__(self, base, dim, device=None):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2, dtype=torch.int64).to(device=device, dtype=torch.float) / dim))
+        self.attention_scaling = 1.0
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.original_inv_freq = self.inv_freq
+
+    @torch.no_grad()
+    def forward(self, x, position_ids):
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1).to(x.device)
+        position_ids_expanded = position_ids[:, None, :].float()
+
+        device_type = x.device.type if isinstance(x.device.type, str) and x.device.type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):  # Force float32
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            cos = emb.cos() * self.attention_scaling
+            sin = emb.sin() * self.attention_scaling
+
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+
+def qwen2_rotate_half(x):
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+def qwen2_apply_rotary_pos_emb(q, cos, sin, position_ids=None, unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (qwen2_rotate_half(q) * sin)
+    return q_embed
+
+@pytest.mark.parametrize(
+    'seq_len, embed_dim, max_seq_len, base', [
+        (256, 32, 1024, 6666),
+        (128, 64, 1024, 8888),
+        (512, 64, 1024, 8888),
+        (888, 64, 1024, 8888),
+    ]
+)
+def test_qwen2_rope(seq_len, embed_dim, max_seq_len, base):
+    rope = QwenRoPE(max_seq_len=max_seq_len, embed_dim=embed_dim, base=base)
+    x = torch.randn(1, seq_len, embed_dim)
+    y = rope(x)
+    rotary_emb = Qwen2RotaryEmbedding(base, embed_dim)
+    position_ids = torch.arange(x.shape[-2]).reshape(1, -1)
+    position_embeddings = rotary_emb(x, position_ids)
+    cos, sin = position_embeddings
+    qwen2_y = qwen2_apply_rotary_pos_emb(x, cos, sin).squeeze(dim=1)
+
+    assert y.shape == qwen2_y.shape
+    assert torch.abs(qwen2_y - y).mean() < 1e-5
