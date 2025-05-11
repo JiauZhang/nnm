3bc196cd8f5e7653a407befd3149f0334a240806
JiauZhang <1743960454@qq.com>
2025-05-11 15:46:19 +0800

    add YaRN

--- nnm/layers/rope.py
+++ nnm/layers/rope.py
@@ -1,6 +1,13 @@
 import math, torch
 from torch import nn
 
+def get_seq_idx(seq_len, cache=None, position=None):
+    if cache:
+        seq_idx = torch.arange(position, position + seq_len)
+    else:
+        seq_idx = torch.arange(seq_len)
+    return seq_idx
+
 class RoPE(nn.Module):
     def __init__(self, *, max_seq_len, embed_dim, base=10000):
         super().__init__()
@@ -23,24 +30,25 @@ class RoPE(nn.Module):
         )
         self.sin = torch.sin(m_theta).to(dtype=torch.float32)
 
-    def seq_idx(self, cache, position, seq_len):
-        if cache:
-            seq_idx = torch.arange(position, position + seq_len)
-        else:
-            seq_idx = torch.arange(seq_len)
-        return seq_idx
-
     def forward(self, x, cache=False, position=None):
         # [..., seq_len, embed_dim]
         shape = x.shape
         assert shape[-1] == self.embed_dim
-        seq_idx = self.seq_idx(cache, position, shape[-2])
+        seq_idx = get_seq_idx(shape[-2], cache, position)
         sin_pe = self.sin[seq_idx, :]
         cos_pe = self.cos[seq_idx, :]
         y = x * cos_pe + x.reshape(-1, 2).flip(dims=[-1]).reshape(shape) * sin_pe
         return y
 
-class QwenRoPE(RoPE):
+class QwenRoPE(nn.Module):
+    def __init__(self, *, max_seq_len, embed_dim, base=10000):
+        super().__init__()
+        assert (embed_dim % 2) == 0, 'embed_dim must be divided by 2'
+        self.max_seq_len = max_seq_len
+        self.embed_dim = embed_dim
+        self.base = base
+        self.precompute()
+
     @torch.no_grad()
     def precompute(self):
         theta = 1.0 / (self.base ** (torch.arange(0, self.embed_dim, 2, dtype=torch.float64) / self.embed_dim))
@@ -57,7 +65,7 @@ class QwenRoPE(RoPE):
     def forward(self, x, cache=False, position=None):
         shape = x.shape
         assert shape[-1] == self.embed_dim
-        seq_idx = self.seq_idx(cache, position, shape[-2])
+        seq_idx = get_seq_idx(shape[-2], cache, position)
         sin_pe = self.sin[seq_idx, :]
         cos_pe = self.cos[seq_idx, :]
         half_embed_dim = shape[-1] // 2
@@ -82,22 +90,21 @@ def linear_ramp_mask(min, max, embed_dim):
     ramp_func = torch.clamp(linear_func, 0, 1)
     return ramp_func
 
-def get_mscale(scale=1):
+def get_temperature(scale=1):
     if scale <= 1:
         return 1.0
     return 0.1 * math.log(scale) + 1.0
 
 class YaRN(nn.Module):
-    def __init__(self, *, max_seq_len, embed_dim, base=10000, scale=1, aplha=1, beta=32, temperature=1):
+    def __init__(self, *, max_seq_len, embed_dim, base=10000, scale=1, alpha=1, beta=32):
         super().__init__()
         assert (embed_dim % 2) == 0, 'embed_dim must be divided by 2'
         self.max_seq_len = max_seq_len
         self.embed_dim = embed_dim
         self.base = base
         self.scale = scale
-        self.alpha = aplha
+        self.alpha = alpha
         self.beta = beta
-        self.temperature = temperature
         self.precompute()
 
     @torch.no_grad()
@@ -108,6 +115,25 @@ class YaRN(nn.Module):
 
         low, high = find_correction_range(self.beta, self.alpha, self.embed_dim, self.base, self.max_seq_len)
         # n-d rotational scaling corrected for extrapolation
-        inv_freq_mask = (1 - linear_ramp_mask(low, high, self.embed_dim // 2).float()) * self.extrapolation_factor
-        self.inv_freq = inv_freq_interpolation * (1 - inv_freq_mask) + inv_freq_extrapolation * inv_freq_mask
-        self.mscale = float(get_mscale(self.scale) * self.attn_factor)
+        inv_freq_mask = 1 - linear_ramp_mask(low, high, self.embed_dim // 2).float()
+        theta = inv_freq_interpolation * (1 - inv_freq_mask) + inv_freq_extrapolation * inv_freq_mask
+        self.temperature = float(get_temperature(self.scale))
+
+        m = torch.arange(self.max_seq_len * self.scale, dtype=torch.float64)
+        m_theta = torch.outer(m, theta)
+        sin_m_theta = torch.sin(m_theta) * self.temperature
+        self.sin = torch.cat([-sin_m_theta, sin_m_theta], dim=-1).to(dtype=torch.float32)
+        self.cos = (torch.cos(m_theta) * self.temperature).to(dtype=torch.float32).repeat(1, 2)
+
+    def forward(self, x, cache=False, position=None):
+        shape = x.shape
+        assert shape[-1] == self.embed_dim
+        seq_idx = get_seq_idx(shape[-2], cache, position)
+        sin_pe = self.sin[seq_idx, :]
+        cos_pe = self.cos[seq_idx, :]
+        half_embed_dim = shape[-1] // 2
+        x1 = x[..., :half_embed_dim]
+        x2 = x[..., half_embed_dim:]
+        y = torch.cat((x2, x1), dim=-1)
+        y = (x * cos_pe) + (y * sin_pe)
+        return y
