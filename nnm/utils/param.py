5695ac5983addb8b6ea10171aeb55e1f1fea10a1
JiauZhang <1743960454@qq.com>
2025-09-03 21:14:43 +0800

    update

--- nnm/utils/param.py
+++ nnm/utils/param.py
@@ -1,10 +1,14 @@
 import numpy as np
 from collections import OrderedDict
 
-def count_parameter(model):
+def count_parameter(model, precision=None, base=None):
     params = model.parameters()
     count = sum([np.prod(p.size()) for p in params])
-    return int(count)
+    if precision:
+        count = format_count(count, precision=precision, base=base)
+    else:
+        count = int(count)
+    return count
 
 def format_count(count, base=None, precision=2):
     base_map = OrderedDict(B=1e9, M=1e6, K=1e3)
